2907d0eaf96db985d73701bf2cbec9043e2076f6
Sonu Sathyadas <sonusathyadas@hotmail.com>
2016-11-17 11:40:51 +0530

    Create and deploy web site
    Create and deploy web site in staging slot. Late you can swap staging
    slot deployments to production slot

---
+++ Create_and_deploy_websites/Script.ps1
@@ -0,0 +1,26 @@
+﻿#Login to the Azure RM A
+Login-AzureRmAccount
+
+#Declare parameter variables
+$resGroup ="masterazure-xxx-RG"
+$appName="sampleapp-xxx"
+$location="South India"
+$slotName="Staging"
+$svcPlanName="appsvc-plan-xxx"
+
+#Create app service plan
+$appSvcPlan = New-AzureRmAppServicePlan -ResourceGroupName $resGroup -Name $svcPlanName -Location $location -Tier Standard #-NumberofWorkers 1 -WorkerSize "Small"
+
+#Create new web app
+New-AzureRmWebApp -ResourceGroupName $resGroup -Name $appName -Location $location -AppServicePlan $appSvcPlan.Name
+
+#Create a deployment slot for staging
+New-AzureRmWebAppSlot -ResourceGroupName $resGroup -Name $appName -Slot $slotName -AppServicePlan $appSvcPlan.Name
+
+#Publish the web site package to staging slot
+Publish-AzureWebsiteProject -Name $appName -Slot $slotName -Package "<path of deployment package>\SampleWebsite.zip"
+
+
+#Swap the Staging slot and production slot
+$ParametersObject = @{targetSlot  = "Production"}
+Invoke-AzureRmResourceAction -ResourceGroupName $resGroup -ResourceType Microsoft.Web/sites/slots -ResourceName $appName/$slotName -Action slotsswap -Parameters $ParametersObject #-ApiVersion 2015-07-01
