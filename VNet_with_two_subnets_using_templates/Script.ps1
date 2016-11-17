44ac899967607214750b3eadf59b8398f77df28b
Sonu Sathyadas <sonusathyadas@hotmail.com>
2016-11-18 00:28:27 +0530

    Create VNET with two subnets using template

---
+++ VNet_with_two_subnets_using_templates/Script.ps1
@@ -0,0 +1,9 @@
+﻿
+#Login to Azure RM account
+Login-AzureRmAccount
+
+#Set your azure subscription if you have multiple subscriptions in your account
+Set-AzureRmContext -SubscriptionName "<subscription name>"
+
+#Create a VNET with two subnets using Deployment template
+New-AzureRmResourceGroupDeployment -ResourceGroupName "<resource-group-name>" -TemplateFile ".\azuredeploy.json" -TemplateParameterFile ".\azuredeploy.parameters.json"
